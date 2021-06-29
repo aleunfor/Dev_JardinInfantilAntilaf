35e8184f8bd6a37c467bcfef3b185b00bda3d2c9
Alejandro Hermosilla <51329486+aleunfor@users.noreply.github.com>
2021-06-28 21:16:00 -0400

    Update keys.js

--- www/keys.js
+++ www/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'jardin_antilaf'
+        host: 'bjprg95ujytw7iv0psyf-mysql.services.clever-cloud.com',
+        user: 'udtmdc0nfnb5mtyp',
+        password: 'kRQQm4qrd4RjjtRCz558',
+        database: 'bjprg95ujytw7iv0psyf'
     }
 }
