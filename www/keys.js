eeb2fea9db08f0b0b9a965ac2d6eaa9f6561df16
Alejandro Hermosilla <alejandro.hermosilla@hotmail.com>
2022-06-28 17:54:51 -0400

    Update keys.js

--- www/keys.js
+++ www/keys.js
@@ -3,10 +3,6 @@ module.exports = {
         host: 'n4m3x5ti89xl6czh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
         user: 'ftps7a84b8zu786r',
         password: 'tft8vth9cqvn8rj2',
-        database: 'fmh080bpzl797wxa',
-        connectTimeout: 60 * 60 * 1000,
-        acquireTimeout: 60 * 60 * 1000,
-        timeout: 60 * 60 * 1000,
-        port: 3306
+        database: 'fmh080bpzl797wxa'
     }
 }
