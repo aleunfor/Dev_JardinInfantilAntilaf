6e42c554ef17bb847769ed50d2d6ab606e075ef7
Alejandro Hermosilla <alejandro.hermosilla@hotmail.com>
2022-06-28 17:08:18 -0400

    Update keys.js

--- www/keys.js
+++ www/keys.js
@@ -3,6 +3,9 @@ module.exports = {
         host: 'containers-us-west-74.railway.app',
         user: 'root',
         password: 'PxBvL0Yhw2mh5kl6pdII',
-        database: 'railway'
+        database: 'railway',
+        connectTimeout: 60 * 60 * 1000,
+        acquireTimeout: 60 * 60 * 1000,
+        timeout: 60 * 60 * 1000,
     }
 }
